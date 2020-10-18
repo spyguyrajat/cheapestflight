d03bdbf1df86757b3d961e94477036f3675b5f0e
Rajat Kumar <rjtkr2@gmail.com>
2020-10-18 15:21:07 +0530

    Update cheapestflight.py
    Request to add custom url.

--- cheapestflight.py
+++ cheapestflight.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 import time
-
+#set range of dates for flight
 for i in range(14,30):
 	try:
 		j = str(i)
+		#get url from google.com/flights with specified conditions like the url above and replace it.
 		url = "https://www.google.com/flights?hl=en#flt=BLR.IXR.2020-10-"+j+";c:INR;e:1;s:0;sd:1;t:f;tt:o"
-		print(url)
+		#print(url)
 		driver = webdriver.Chrome()
 		driver.get(url)
 		time.sleep(5)
